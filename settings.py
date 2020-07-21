bc31b4bd0fdc989c3e4bd288af2e55e7f1840208
Will-Hoey <will.hoey@hotmail.co.uk>
2020-07-21 15:59:02 +0100

    Add settings to allow running migrations with django-admin

--- settings.py
+++ settings.py
@@ -1,7 +1,14 @@
 
 SECRET_KEY = "giant-plugins"
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "giant-plugins",
+    }
+}
+
 
 INSTALLED_APPS = [
     "cms",
@@ -16,10 +23,11 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sites",
+    "django.contrib.sessions",
+    "django.contrib.messages",
     "mixins",
     "giant_plugins",
 ]
-ROOT_URLCONF = "giant_plugins.tests.urls"
 
 TEMPLATES = [
     {
@@ -27,7 +35,9 @@ TEMPLATES = [
         "DIRS": ["giant_plugins/templates"],
         "OPTIONS": {
             "context_processors": [
+                "django.contrib.auth.context_processors.auth",
                 "django.template.context_processors.request",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
@@ -36,6 +46,7 @@ TEMPLATES = [
 MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
 ]
 
 SITE_ID = 1
