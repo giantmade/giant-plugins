5d7b08e529c813558c755cfa9352e9d954d3e8b0
Will-Hoey <will.hoey@hotmail.co.uk>
2020-07-29 15:09:35 +0100

    Update to use try/except block. Remove list comprehension

--- giant_plugins/cms_plugins.py
+++ giant_plugins/cms_plugins.py
@@ -1,16 +1,16 @@
-from django.conf import settings
 from django.utils.module_loading import import_string
+from cms.exceptions import PluginAlreadyRegistered
 
-include_list = [
-        "content_width_video",
-        "content_width_image",
-        "page_card",
-        "pullquote",
-        "rich_text",
-    ]
-exclude_list = getattr(settings, "GIANT_PLUGINS_EXCLUDE_LIST", [])
-imports_list = [plugin for plugin in include_list if plugin not in exclude_list]
+import_list = [
+    "content_width_video",
+    "content_width_image",
+    "page_card",
+    "pullquote",
+    "rich_text",
+]
 
-
-for plugin in imports_list:
-    import_string(f"giant_plugins.{plugin}.cms_plugins.__all__")
+for plugin in import_list:
+    try:
+        import_string(f"giant_plugins.{plugin}.cms_plugins.__all__")
+    except PluginAlreadyRegistered:
+        pass
