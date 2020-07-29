4e9201d5650d8bef07871ff20a00f5d041e745e6
Will-Hoey <will.hoey@hotmail.co.uk>
2020-07-29 14:47:58 +0100

    Change to exclude from list rather than include

--- giant_plugins/cms_plugins.py
+++ giant_plugins/cms_plugins.py
@@ -1,17 +1,16 @@
 from django.conf import settings
 from django.utils.module_loading import import_string
 
-imports_list = getattr(
-    settings,
-    "GIANT_PLUGINS_LIST",
-    [
+include_list = [
         "content_width_video",
         "content_width_image",
         "page_card",
         "pullquote",
         "rich_text",
-    ],
-)
+    ]
+exclude_list = getattr(settings, "GIANT_PLUGINS_EXCLUDE_LIST", [])
+imports_list = [plugin for plugin in include_list if plugin not in exclude_list]
+
 
 for plugin in imports_list:
     import_string(f"giant_plugins.{plugin}.cms_plugins.__all__")
