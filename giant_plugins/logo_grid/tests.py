252ef955835e41a421458e476028e9d6652ea0c7
Will-Hoey <will.hoey@hotmail.co.uk>
2021-05-20 14:01:28 +0100

    Fix test

--- giant_plugins/logo_grid/tests.py
+++ giant_plugins/logo_grid/tests.py
@@ -18,13 +18,6 @@ class TestLogoModels:
         plugin = models.LogoCard(pk=1)
         assert str(plugin) == "Logo card 1"
 
-    def test_absolute_url_no_link(self):
-        """
-        Test bool is false when no link is set
-        """
-        plugin = models.LogoCard()
-        assert plugin.get_absolute_url() == ""
-        assert bool(plugin.get_absolute_url()) is False
 
     def test_absolute_url_link(self):
         """
